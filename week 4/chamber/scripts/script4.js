abee32be9d20d9c7f42cefb9ec1f62d28a481b2c
Danicd7 <danielsilva_88@outlook.com>
2022-11-11 15:00:59 +0100

    commit

--- week 4/chamber/scripts/script4.js
+++ week 4/chamber/scripts/script4.js
@@ -1,3 +1,11 @@
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Update: ${document.lastModified}`;
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
